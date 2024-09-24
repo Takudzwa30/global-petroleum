2511b3ade74afc5079dd4681e81637247008908a
Takudzwa Allen <takudzwamushai@gmail.com>
2024-09-24 16:04:14 +0400

    added section wrapper for the whole project

---
+++ src/components/SectionWrapper/SectionWrapper.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+// Styles
+import Style from "./SectionWrapper.module.css";
+
+const SectionWrapper = () => {
+  return <div className={Style.sectionWrapper}>SectionWrapper</div>;
+};
+
+export default SectionWrapper;
